25c42438fb07a952b4046b3529aa3593ac6e9311
PedroGtz16 <37647681+PedroGtz16@users.noreply.github.com>
2018-10-24 16:58:16 -0500

    Add files via upload

--- src/main/java/mx/iteso/icecreams/milk/MangoMilkIceCream.java
+++ src/main/java/mx/iteso/icecreams/milk/MangoMilkIceCream.java
@@ -1,6 +1,8 @@
 package mx.iteso.icecreams.milk;
 
 import mx.iteso.icecreams.IceCream;
-
+/**
+ * milk mango.
+ */
 public class MangoMilkIceCream extends IceCream {
 }
--- src/main/java/mx/iteso/icecreams/milk/PineappleMilkIceCream.java
+++ src/main/java/mx/iteso/icecreams/milk/PineappleMilkIceCream.java
@@ -1,6 +1,8 @@
 package mx.iteso.icecreams.milk;
 
 import mx.iteso.icecreams.IceCream;
-
+/**
+ * milk pinea.
+ */
 public class PineappleMilkIceCream extends IceCream {
 }
