ffeb109dbfbc7857db3d2f26acf19048364f1ab9
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-10-24 21:28:37 -0500

    finish
    listo

--- src/main/java/mx/iteso/Cone.java
+++ src/main/java/mx/iteso/Cone.java
@@ -2,15 +2,27 @@ package mx.iteso;
 
 import mx.iteso.icecreams.IceCream;
 
+/**
+ * EClass Cone.
+ */
 public class Cone {
+    /**
+     * Met.
+     */
     public void prepare() {
 
     }
-
-    public void addIceCream(IceCream iceCream) {
+    /**
+     * Met.
+     * @param iceCream .
+     */
+    public void addIceCream(final IceCream iceCream) {
 
     }
 
+    /**
+     * Met..
+     */
     public void serve() {
 
     }
